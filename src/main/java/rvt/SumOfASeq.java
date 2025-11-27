28932264e76743a760bc498ae682d0846e8158c8
24DP3MLaiz <A240144ML@rvt.lv>
2025-11-27 12:27:14 +0200

    Sum of a sequence Java-mooc: 2-3

---
+++ src/main/java/rvt/SumOfASeq.java
@@ -0,0 +1,22 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class SumOfASeq {
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ievadi skaitli:");
+        int n = Integer.valueOf(sc.nextLine());
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Last number? " + n);
+        System.out.println("The sum is " + sum);
+    }
+}
